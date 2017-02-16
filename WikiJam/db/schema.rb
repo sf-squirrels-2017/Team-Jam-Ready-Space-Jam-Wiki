12f0192b0ab49a99d7545e45f17cd0d5c95b0e97
Rebecca Orr <wholemilklover@gmail.com>
2017-02-16 10:19:02 -0800

    Adds current_id to articles table.

--- WikiJam/db/schema.rb
+++ WikiJam/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216000144) do
+ActiveRecord::Schema.define(version: 20170216002800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170216000144) do
     t.integer  "category_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "current_edit_id"
     t.index ["category_id"], name: "index_articles_on_category_id", using: :btree
     t.index ["creator_id"], name: "index_articles_on_creator_id", using: :btree
   end
@@ -62,4 +63,5 @@ ActiveRecord::Schema.define(version: 20170216000144) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "articles", "edits", column: "current_edit_id"
 end
