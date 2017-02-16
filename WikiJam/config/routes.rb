0c64c5f678637744c11f7ff4abae952e2dd10fca
Lisa Nguyen <lisathuanguyen@gmail.com>
2017-02-16 15:28:15 -0800

    setting categories#index as root page

--- WikiJam/config/routes.rb
+++ WikiJam/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :users, only: [:new, :create, :destroy]
   get '/join' => 'users#new'
-  root to: 'sessions#new'
+  root to: 'scategories#index'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
