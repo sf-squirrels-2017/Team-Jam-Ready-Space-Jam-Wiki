04acb483335f60f1cb3997d8e9c6ee021f789aa0
Lisa Nguyen <lisathuanguyen@gmail.com>
2017-02-16 15:20:42 -0800

    index & show route for category

--- WikiJam/config/routes.rb
+++ WikiJam/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create, :destroy] 
+  resources :users, only: [:new, :create, :destroy]
   get '/join' => 'users#new'
-  root to: 'sessions#new'  
+  root to: 'sessions#new'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  
+
+  resources :categories , only: [:index, :show]
 end
