ec0b4607b6aed7b062da0204e72d77470fc63914
Rebecca Orr <wholemilklover@gmail.com>
2017-02-16 15:09:26 -0800

    Adds login and logout controller, routes and helpers.

--- WikiJam/config/routes.rb
+++ WikiJam/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :destroy] 
   get '/join' => 'users#new'
-  root to: 'users#new'  
+  root to: 'sessions#new'  
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   
 end
