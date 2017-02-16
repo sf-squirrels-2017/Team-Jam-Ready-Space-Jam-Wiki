f2ef47c763e21aa8e3765a881fb663bfa76837af
Rebecca Orr <wholemilklover@gmail.com>
2017-02-16 14:55:15 -0800

    Makes user registration.

--- WikiJam/app/controllers/users_controller.rb
+++ WikiJam/app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user && @user.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
 
+
+
+  private 
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :password_confirmation)
+  end
 end
