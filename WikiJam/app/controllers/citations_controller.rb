4626d5791f96182410ce40e6c0b77c71049777b7
Lisa Nguyen <lisathuanguyen@gmail.com>
2017-02-16 21:02:31 -0800

    new citation form and displaying all citations

--- WikiJam/app/controllers/citations_controller.rb
+++ WikiJam/app/controllers/citations_controller.rb
@@ -1,7 +1,25 @@
 class CitationsController < ApplicationController
+  # before_action :find_article
 
   def index
     @citations = Citation.all
   end
 
+  def new
+    @citation = Citation.new
+  end
+
+  def create
+    @citation = @article.citaions.new(citation_params)
+    @article.creator_id = current_user.id
+  end
+
+  private
+  # def find_article
+  #   @article = Article.find(params[:article_id])
+  # end
+
+  def citation_params
+    params.require(:citation).permit(:citation_body)
+  end
 end
--- WikiJam/config/routes.rb
+++ WikiJam/config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
     resources :articles
   end
 
+  resources :citations
 
 end
